1b800d2f6d380c51b65c49fade69150d062c85d1
to-kra <tomas.kramaric@gmail.com>
2017-04-18 14:11:06 +0200

    fix: if remote upstream was not added before

--- bin/gitSyncWork.sh
+++ bin/gitSyncWork.sh
@@ -7,7 +7,28 @@ BWhite='\x1B[1;37m'       # White
 BRed='\x1B[1;31m'         # Red
 BGreen='\x1B[1;32m'       # Green
 
+function existRemote () {
+    cd $repository
+    remoteList=`git remote -v`
+    if [[ $remoteList == *"$1"* ]]; then
+        #echo "Tap: $1 exists..."
+        return 0;
+    else
+        #echo "Tap: $1 does not exist !!!"
+    return 1;
+  fi
+}
+
 echo -e "Starting ${BWhite}synchronizing${NC} '${BRed}master${NC}' & '${BRed}dev${NC}' with ${BWhite}upstream KC2.0${NC}..."
+
+if ! existRemote 'upstream' ; then
+    echo -e "\t${BRed}>>> FATAL:${NC} Remote ${BWhite}upstream${NC} does not exist ${BRed}!!!${NC}"
+    echo -e "\tAdd remote with:"
+    echo -e "\t\tgit remote add <REMOTE_NAME> <REMOTE_URL>"
+    echo -e "\t\tgit remote add upstream git@github.ibm.com:IBMKC/KC2.0.git"
+    exit 1
+fi
+
 cd $repository
 git fetch upstream
 
@@ -31,6 +52,6 @@ git pull upstream dev
 # ?? maybe not needed
 git merge upstream dev
 echo -e "\n> ${BWhite}Push${NC} '${BRed}local/dev${NC}' to '${BRed}origin/dev${NC}'"
-git push origin dev
+git push -u origin dev
 
 echo -e "\nStarting ${BWhite}synchronizing${NC} '${BRed}master${NC}' & '${BRed}dev${NC}' with ${BWhite}upstream KC2.0${NC}... ${BGreen}done${NC}"
