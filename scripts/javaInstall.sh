2c1c9e3f5f9a9a84229c0548c4fbd7dafbcb0884
to-kra <tomas.kramaric@gmail.com>
2017-06-20 14:10:19 +0200

    description for future update of java installation

--- scripts/javaInstall.sh
+++ scripts/javaInstall.sh
@@ -20,6 +20,9 @@ source $my_dir/ubuntuOsTools.sh
 
 # Java
 if isMacOs ; then
+    # ll Library/Java/JavaVirtualMachines/ -> will list all java dirs
+    # then use: greadlink -f jdk1.8.0_112.jdk -> to get real paths
+    # then add them into jenv: jenv add /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home oracle64-1.6.0.39 added
     export JAVA_HOME=$(/usr/libexec/java_home)
 fi
 if isUbuntu ; then
