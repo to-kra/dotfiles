5d138b15572d2d670b4197d63043796619f82b4c
to-kra <tomas.kramaric@gmail.com>
2017-07-03 11:30:59 +0200

    refactor + added new branches

--- bin/gitSyncWork.sh
+++ bin/gitSyncWork.sh
@@ -5,7 +5,8 @@ repo_name="KC2.0"
 repo_path=${local_repos_dir}/${repo_name}
 branch_remote_tracking="origin"
 git_remote="upstream"
-declare -a branches=("master" "dev" "release")
+default_branch="dev"
+declare -a branches=("master" "$default_branch" "release" "disqus" "video-search")
 
 # Colors
 NC="\x1B[m"               # Color Reset
@@ -20,12 +21,10 @@ function existRemote () {
     cd $repository
     local remoteList=`git remote -v`
     if [[ $remoteList == *"$1"* ]]; then
-        #echo "Tap: $1 exists..."
         return 0;
     else
-        #echo "Tap: $1 does not exist !!!"
-    return 1;
-  fi
+        return 1;
+    fi
 }
 
 function arrayJoinBy {
@@ -42,13 +41,58 @@ function printlnSeparator {
 }
 
 function printVars {
-    echo -e "\nlocal_repos_dir        : $local_repos_dir"
-    echo "repo_name              : $repo_name"
-    echo "repo_path              : $repo_path"
-    echo "branches               : `arrayJoinBy "," branches[@]`"
-    echo "branch_remote_tracking : $branch_remote_tracking"
-    echo "git_remote             : $git_remote"
-    echo ""
+    echo -e "\n-------------------------------------------------------------"
+    echo -e "| local_repos_dir        : ${BWhite}${local_repos_dir}${NC}"
+    echo -e "| repo_name              : ${BWhite}${repo_name}${NC}"
+    echo -e "| repo_path              : ${BWhite}${repo_path}${NC}"
+    echo -e "| branches               : ${BWhite}`arrayJoinBy "," branches[@]`${NC}"
+    echo -e "| branch_remote_tracking : ${BWhite}${branch_remote_tracking}${NC}"
+    echo -e "| git_remote             : ${BWhite}${git_remote}${NC}"
+    echo -e "-------------------------------------------------------------\n"
+}
+
+function printMissingRemote {
+    local remote="$1"
+    echo -e "\t${BRed}>>> FATAL:${NC} Remote ${BWhite}${remote}${NC} does not exist ${BRed}!!!${NC}"
+    echo -e "\tAdd remote with:"
+    echo -e "\t\tgit remote add <REMOTE_NAME> <REMOTE_URL>"
+    echo -e "\t\tgit remote add ${remote} git@github.ibm.com:IBMKC/${repo_name}.git"
+}
+
+function gitCheckout {
+    local branch="$1"
+    echo -e "\n> ${BWhite}Checking out${NC} '${BRed}${branch}${NC}'"
+    git checkout ${branch}
+}
+
+function gitFetch {
+    local remote="$1"
+    echo -e "\n> ${BWhite}Fetching${NC} from '${BRed}${remote}${NC}'"
+    git fetch ${remote}
+}
+
+function gitPull {
+    local remote="$1"
+    local branch="$2"
+    echo -e "\n> ${BWhite}Pulling${NC} '${BRed}${branch}${NC}' from '${BRed}${remote}${NC}'"
+    git pull ${remote} ${branch}
+}
+
+function gitPush {
+    local remote="$1"
+    local branch="$2"
+    echo -e "\n> ${BWhite}Pushing${NC} '${BRed}${branch}${NC}' to '${BRed}${remote}${NC}'"
+    git push -u ${remote} ${branch}
+}
+
+function synchBranch {
+    local branch="$1"
+    local remote_fork="$2"
+    local remote_project="$3"
+    gitFetch ${remote_project}
+    gitCheckout ${branch}
+    gitPull ${remote_project} ${branch}
+    gitPush ${remote_fork} ${branch}
 }
 
 # ========================
@@ -61,35 +105,24 @@ printlnSeparator
 # print input variables
 printVars
 cd ${repo_path}
-echo -e "${BWhite}Synchronization${NC} '${BGreen}master${NC}' & '${BGreen}dev${NC}' & '${BGreen}release${NC}' with ${BWhite}upstream KC2.0${NC}... ${BGreen}started${NC}"
+echo -e "${BWhite}Synchronization${NC} of '${BGreen}`arrayJoinBy "," branches[@]`${NC}' with ${BWhite}${git_remote} ${repo_name}${NC}... ${BGreen}started${NC}"
 
 # check for remote upstream in local repo
-if ! existRemote "$git_remote" "`pwd`" ; then
-    echo -e "\t${BRed}>>> FATAL:${NC} Remote ${BWhite}upstream${NC} does not exist ${BRed}!!!${NC}"
-    echo -e "\tAdd remote with:"
-    echo -e "\t\tgit remote add <REMOTE_NAME> <REMOTE_URL>"
-    echo -e "\t\tgit remote add upstream git@github.ibm.com:IBMKC/${repo_name}.git"
+if ! existRemote "${git_remote}" "`pwd`" ; then
+    printMissingRemote "${git_remote}"
     exit 1
 fi
 
 # sync of all branches
 for branch in "${branches[@]}"
 do
-    echo -e "\n${BYellow}Start${NC} ${BWhite}synchronizing${NC} '${BRed}${branch}${NC}' of '${BRed}${repo_name}${NC}' with '${BRed}${git_remote}${NC}'..."
-
-    echo -e "\n> ${BWhite}Fetching${NC} from '${BRed}${git_remote}${NC}'"
-    git fetch ${git_remote}
-
-    echo -e "\n> ${BWhite}Checking out${NC} '${BRed}${branch}${NC}'"
-    git checkout ${branch}
-
-    echo -e "\n> ${BWhite}Pulling${NC} '${BRed}${branch}${NC}' from '${BRed}${git_remote}${NC}'"
-    git pull ${git_remote} ${branch}
-
-    echo -e "\n> ${BWhite}Pushing${NC} '${BRed}${branch}${NC}' to '${BRed}${branch_remote_tracking}${NC}'"
-    git push -u ${branch_remote_tracking} ${branch}
-
-    echo -e "\nFinished ${BWhite}synchronizing${NC} '${BRed}${branch}${NC}' of '${BRed}${repo_name}${NC}' with '${BRed}${git_remote}${NC}'..."
+    echo -e "\n${BYellow}Start${NC} ${BWhite}synchronizing${NC} '${BGreen}${branch}${NC}' of '${BRed}${repo_name}${NC}' with '${BRed}${git_remote}${NC}'..."
+    synchBranch "${branch}" "${branch_remote_tracking}" "${git_remote}"
+    echo -e "\nFinished ${BWhite}synchronizing${NC} '${BWhite}${branch}${NC}' of '${BRed}${repo_name}${NC}' with '${BRed}${git_remote}${NC}'..."
 done
 
-echo -e "\n${BWhite}Synchronization${NC} '${BGreen}master${NC}' & '${BGreen}dev${NC}' & '${BGreen}release${NC}' with ${BWhite}upstream KC2.0${NC}... ${BGreen}finished${NC}"
+# checkout default branch at the end
+echo -e "\n> ${BWhite}Checking out${NC} default branch: '${BRed}${default_branch}${NC}'"
+git checkout ${default_branch}
+
+echo -e "\n${BWhite}Synchronization${NC} of '${BGreen}`arrayJoinBy "," branches[@]`${NC}' with ${BWhite}${git_remote} ${repo_name}${NC}... ${BGreen}finished${NC}"
