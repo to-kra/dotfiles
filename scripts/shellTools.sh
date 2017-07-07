a95dc8fd171e4ed367f8eeac2d1df2bb47de6b4d
to-kra <tomas.kramaric@gmail.com>
2017-07-07 14:13:58 +0200

    update

--- scripts/shellTools.sh
+++ scripts/shellTools.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
 function stringContains() {
-    FULL_STRING=$1
-    STRING_TO_FIND=$2
+    local FULL_STRING=$1
+    local STRING_TO_FIND=$2
     if [[ $FULL_STRING == *"$STRING_TO_FIND"* ]] ; then
         return 0
     fi
     return 1
+}
+
+function stringEquals {
+    local str1=$1
+    local str2=$2
+    if [ "$str1" == "$str2" ]; then
+        return 0
+    fi
+    return 1
+}
+
+function arrayJoinBy {
+    local separator=$1
+    local array=("${!2}")
+    IFS=$separator
+    echo "${array[*]// /|}"
+    IFS=$' \t\n'
 }
